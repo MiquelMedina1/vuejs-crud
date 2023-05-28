49701b5773379266acd713c51387707e3f7f1a45
Miquel Medina <miquel.medina@estudiants.urv.cat>
2023-05-28 20:45:35 +0200

    crypto cart added

--- src/assets/js/components/all-cryptos.vue
+++ src/assets/js/components/all-cryptos.vue
@@ -11,6 +11,9 @@
             <button class ="btn btn-info" @click="ordenarCrypto">{{ sortDirection }}</button>
         </div>
 
+        <button class="btn btn-primary" @click="showCart">Open Cart</button>
+
+
         <!-- Crypto info -->
         <div class="crypto-grid">
             <div v-for="crypto in cryptos" :key="crypto.id" class="crypto">
@@ -34,11 +37,34 @@
             </div>
         </div>
 
-        <!-- Cart -->
-        <div class="cart-container">
-            
+        <!-- CartModal -->
+        <div v-if="cartVisible" class="cart-modal">
+            <div class="cart-content">
+                <h2>Your Cart</h2>
+                <div v-if="cart.length === 0">
+                    <p>Your cart is empty.</p>
+                </div>
+                <div v-else>
+                    <div v-for="item in cart" :key="item.id" class="cart-item">
+                        <img :src="`/src/assets/img/${item.crypto.abbreviation}.png`" class="cart-item-image" alt="Crypto Image">
+                        <div class="cart-item-details">
+                            <span class="cart-item-name">{{ item.crypto.name }}</span>
+                            <span class="cart-item-price">Price: €{{ item.crypto.lastQuote }}</span>
+                            <input type="number" v-model="item.quantity" class="cart-item-quantity" min="1">
+                        </div>
+                        <div class="cart-item-actions">
+                            <button @click="removeFromCart(item)" class="btn btn-sm btn-warning">Remove 🗑️</button>
+                        </div>
+                    </div>
+                    <div class="cart-total">
+                        <span class="cart-total-label">Total:</span>
+                        <span class="cart-total-price">€{{ calculateTotalPrice }}</span>
+                    </div>
+                </div>
+                <button class="btn btn-primary" @click="closeCart">Close</button>
+            </div>
         </div>
-
+    
     </div>
 </template>
 
@@ -52,7 +78,9 @@
                 cryptoSearch: '',
                 sortDirection: 'Asc 🡡',
                 rows: 3,
-                columns: 3
+                columns: 3,
+                cart : [],
+                cartVisible: false
             }
         },
 
@@ -61,21 +89,61 @@
             this.fetchCryptoData();
         },
 
+        computed: {
+            calculateTotalPrice() {
+            var totalPrice = 0;
+            for (const item of this.cart) {
+                const price = parseFloat(item.crypto.lastQuote);
+                const quantity = item.quantity;
+                totalPrice += price * quantity;
+        }
+        return totalPrice.toFixed(2);
+            },
+        },
+
         methods: {
-            ordenarCrypto: function()
-            {
-                if (this.sortDirection === 'Asc 🡡') {
-                    this.cryptos.sort((a, b) => (a.price > b.price) ? 1 : -1)
-                    this.sortDirection = 'Desc 🡣'
-                }else {
-                    this.cryptos.sort((a, b) => (a.price < b.price) ? 1 : -1)
-                    this.sortDirection = 'Asc 🡡'
+            addToCart: function(crypto) {
+                const existingItem = this.cart.find(item => item.crypto.abbreviation === crypto.abbreviation);
+                if (existingItem) {
+                    existingItem.quantity++;
+                } else {
+                    this.cart.push({ crypto, quantity: 1 });
                 }
             },
 
-            addToCart: function(crypto)
-            {
+            closeCart: function() {
+                this.cartVisible = false;
+            },
 
+            ordenarCrypto: function()
+            {
+                if (this.sortDirection === 'Asc 🡡') {
+                    this.cryptos.sort((a, b) => (a.lastQuote > b.lastQuote) ? 1 : -1)
+                    this.sortDirection = 'Desc 🡣'
+                }else {
+                    this.cryptos.sort((a, b) => (a.lastQuote < b.lastQuote) ? 1 : -1)
+                    this.sortDirection = 'Asc 🡡'
+                }
+            },
+            showCart() {
+                this.cartVisible = true;
+            },
+
+            incrementQuantity: function(item) {
+                item.quantity++;
+            },
+            
+            decrementQuantity: function(item) {
+                if (item.quantity > 1) {
+                    item.quantity--;
+                }
+            },
+
+            removeFromCart: function(item) {
+                const index = this.cart.indexOf(item);
+                if (index > -1) {
+                    this.cart.splice(index, 1);
+                }
             },
 
             fetchCryptoData: function()
